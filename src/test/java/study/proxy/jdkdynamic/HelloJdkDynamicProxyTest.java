1d2ca1a6cdb31ec5001abf5e7c602d19f9285d93
Jinho Yim <jinhoyim@outlook.com>
2019-09-08 02:53:43 +0900

    feat: 람다 핸들러를 사용한 JDK Dynamic Proxy 추가

--- src/test/java/study/proxy/jdkdynamic/HelloJdkDynamicProxyTest.java
+++ src/test/java/study/proxy/jdkdynamic/HelloJdkDynamicProxyTest.java
@@ -3,6 +3,7 @@ package study.proxy.jdkdynamic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,20 +11,36 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class HelloJdkDynamicProxyTest {
 
+    @DisplayName("람다 핸들러를 적용하여 모든 메서드 대문자 반환")
+    @Test
+    void toUpperProxyWithLambda() {
+        toUpperProxy((proxy, method, args) -> {
+            Object result = method.invoke(new HelloTarget(), args);
+            if (method.getReturnType() == String.class) {
+                return ((String) result).toUpperCase();
+            }
+            return result;
+        });
+    }
+
     @DisplayName("핸들러 객체를 사용한 프록시를 적용하여 모든 메서드 대문자 반환")
     @Test
-    void toUpperProxy() {
-        HelloTarget target = new HelloTarget();
+    void toUpperProxyWithInstanceOfHandler() {
+        toUpperProxy(new ToUpperInvocationHandler(new HelloTarget()));
+    }
+
+    private void toUpperProxy(InvocationHandler invocationHandler) {
         Hello proxyInstance = (Hello) Proxy.newProxyInstance(
                 HelloJdkDynamicProxyTest.class.getClassLoader(),
                 new Class[]{Hello.class},
-                new ToUpperInvocationHandler(target));
+                invocationHandler);
         String name = "J";
+        HelloTarget expected = new HelloTarget();
 
         assertAll("모든 메서드 대문자 반환",
-                () -> assertThat(proxyInstance.sayHello(name)).isEqualTo(target.sayHello(name).toUpperCase()),
-                () -> assertThat(proxyInstance.sayHi(name)).isEqualTo(target.sayHi(name).toUpperCase()),
-                () -> assertThat(proxyInstance.sayThankYou(name)).isEqualTo(target.sayThankYou(name).toUpperCase())
+                () -> assertThat(proxyInstance.sayHello(name)).isEqualTo(expected.sayHello(name).toUpperCase()),
+                () -> assertThat(proxyInstance.sayHi(name)).isEqualTo(expected.sayHi(name).toUpperCase()),
+                () -> assertThat(proxyInstance.sayThankYou(name)).isEqualTo(expected.sayThankYou(name).toUpperCase())
         );
     }
 }
