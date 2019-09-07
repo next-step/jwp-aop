9bdfe81761fcee22a1eb6407006914a40f9b7fcc
Jinho Yim <jinhoyim@outlook.com>
2019-09-08 04:39:04 +0900

    refactor: 불필요한 멤버 코드 제거

--- src/test/java/study/proxy/jdkdynamic/ToUpperInvocationHandler.java
+++ src/test/java/study/proxy/jdkdynamic/ToUpperInvocationHandler.java
@@ -1,30 +1,21 @@
 package study.proxy.jdkdynamic;
 
-import com.google.common.collect.Maps;
-import org.reflections.ReflectionUtils;
 import study.proxy.MethodResultHelper;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Set;
 
 class ToUpperInvocationHandler implements InvocationHandler {
 
     private final Object target;
-    private final Map<String, Method> methodMap = Maps.newHashMap();
 
     ToUpperInvocationHandler(Object target) {
         this.target = target;
-        Set<Method> methodSet = ReflectionUtils.getMethods(target.getClass());
-        for (Method method : methodSet) {
-            methodMap.put(method.getName(), method);
-        }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Object result = methodMap.get(method.getName()).invoke(target, args);
+        Object result = method.invoke(target, args);
         return MethodResultHelper.toUpper(method, result);
     }
 }
