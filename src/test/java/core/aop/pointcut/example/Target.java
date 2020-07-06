ae707a19fcaef46bacd0c05e351b1b9143a2033b
nokchax <nokchax@gmail.com>
2020-07-06 16:27:09 +0900

    test: method match point cut test

---
+++ src/test/java/core/aop/pointcut/example/Target.java
@@ -0,0 +1,13 @@
+package core.aop.pointcut.example;
+
+public class Target {
+    private final String string = "string";
+
+    public String getString() {
+        return string;
+    }
+
+    public String stringValue() {
+        return string;
+    }
+}
