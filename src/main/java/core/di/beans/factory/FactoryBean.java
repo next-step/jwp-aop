85e89fe1f13aabc7ed607776c4e2d527755bdb1d
hyeyoom <neoul_chw@icloud.com>
2020-07-06 22:25:07 +0900

    feat: FactoryBean 정의

---
+++ src/main/java/core/di/beans/factory/FactoryBean.java
@@ -0,0 +1,29 @@
+package core.di.beans.factory;
+
+import org.springframework.lang.Nullable;
+
+/**
+ * @param <T> the bean type
+ */
+public interface FactoryBean<T> {
+
+    /**
+     * @return An instance of the object that managed by this factory.
+     * @throws Exception 장비를 정지합니다. 왜 얘만 한글이냐.
+     */
+    @Nullable
+    T getObject() throws Exception;
+
+    /**
+     * @return the type of object
+     */
+    @Nullable
+    Class<?> getObjectType();
+
+    /**
+     * @return true if {@link #getObject} always return the same object.
+     */
+    default boolean isSingleton() {
+        return true;
+    }
+}
