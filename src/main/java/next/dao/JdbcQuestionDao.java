35cf85ce8ac694a3b41fe8a188d69e8d74e82912
hyeyoom <neoul_chw@icloud.com>
2020-07-09 23:19:11 +0900

    bugfix: 기존 오동작 수정

--- src/main/java/next/dao/JdbcQuestionDao.java
+++ src/main/java/next/dao/JdbcQuestionDao.java
@@ -23,16 +23,13 @@ public class JdbcQuestionDao implements QuestionDao {
     @Override
     public Question insert(Question question) {
         String sql = "INSERT INTO QUESTIONS (writer, title, contents, createdDate) VALUES (?, ?, ?, ?)";
-        PreparedStatementCreator psc = new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                PreparedStatement pstmt = con.prepareStatement(sql);
-                pstmt.setString(1, question.getWriter());
-                pstmt.setString(2, question.getTitle());
-                pstmt.setString(3, question.getContents());
-                pstmt.setTimestamp(4, new Timestamp(question.getTimeFromCreateDate()));
-                return pstmt;
-            }
+        PreparedStatementCreator psc = con -> {
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, question.getWriter());
+            pstmt.setString(2, question.getTitle());
+            pstmt.setString(3, question.getContents());
+            pstmt.setTimestamp(4, new Timestamp(question.getTimeFromCreateDate()));
+            return pstmt;
         };
 
         KeyHolder keyHolder = new KeyHolder();
@@ -45,14 +42,8 @@ public class JdbcQuestionDao implements QuestionDao {
         String sql = "SELECT questionId, writer, title, createdDate, countOfAnswer FROM QUESTIONS "
                 + "order by questionId desc";
 
-        RowMapper<Question> rm = new RowMapper<Question>() {
-            @Override
-            public Question mapRow(ResultSet rs) throws SQLException {
-                return new Question(rs.getLong("questionId"), rs.getString("writer"), rs.getString("title"), null,
-                        rs.getTimestamp("createdDate"), rs.getInt("countOfAnswer"));
-            }
-
-        };
+        RowMapper<Question> rm = rs -> new Question(rs.getLong("questionId"), rs.getString("writer"), rs.getString("title"), null,
+                rs.getTimestamp("createdDate"), rs.getInt("countOfAnswer"));
 
         return jdbcTemplate.query(sql, rm);
     }
@@ -62,13 +53,8 @@ public class JdbcQuestionDao implements QuestionDao {
         String sql = "SELECT questionId, writer, title, contents, createdDate, countOfAnswer FROM QUESTIONS "
                 + "WHERE questionId = ?";
 
-        RowMapper<Question> rm = new RowMapper<Question>() {
-            @Override
-            public Question mapRow(ResultSet rs) throws SQLException {
-                return new Question(rs.getLong("questionId"), rs.getString("writer"), rs.getString("title"),
-                        rs.getString("contents"), rs.getTimestamp("createdDate"), rs.getInt("countOfAnswer"));
-            }
-        };
+        RowMapper<Question> rm = rs -> new Question(rs.getLong("questionId"), rs.getString("writer"), rs.getString("title"),
+                rs.getString("contents"), rs.getTimestamp("createdDate"), rs.getInt("countOfAnswer"));
 
         return jdbcTemplate.queryForObject(sql, rm, questionId);
     }
