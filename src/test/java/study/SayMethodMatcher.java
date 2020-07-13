5297d26b85e43410edfdd9969ebefc5f2b5370f1
parkeeseul <eeseul.p@gmail.com>
2020-07-14 02:42:37 +0900

    test: SayMethodMatcher 수정

--- src/test/java/study/SayMethodMatcher.java
+++ src/test/java/study/SayMethodMatcher.java
@@ -6,9 +6,6 @@ import java.lang.reflect.Method;
 public class SayMethodMatcher implements MethodMatcher {
     @Override
     public boolean matches(Method m, Class targetClass, Object[] args) {
-        if(m.getName().startsWith("say")) {
-            return true;
-        }
-        return false;
+        return m.getName().startsWith("say");
     }
 }
