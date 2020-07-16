e2850f105cad9415303431041ab12f80654d7ecc
박동엽 <ninjasul@onestorecorp.com>
2020-07-16 16:15:22 +0900

    test: @ParameterizedTest 로 수정

--- src/test/java/core/mvc/ControllerAdviceTest.java
+++ src/test/java/core/mvc/ControllerAdviceTest.java
@@ -12,7 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 import next.config.MyConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -54,31 +57,20 @@ public class ControllerAdviceTest {
         response = new MockHttpServletResponse();
     }
 
-    @Test
-    void singleExceptionHandler() throws Exception {
-        String requestURI = "/test/controllerAdvice/firstSampleException";
-        Class<? extends Throwable> targetThrowable = FirstSampleException.class;
-
-        assertExceptionHandler(requestURI, targetThrowable);
+    @DisplayName("@ControllerAdvice 기능 테스트")
+    @ParameterizedTest(name = "{index} - requestURI={0}, targetClassName={1}")
+    @CsvSource({
+        "'/test/controllerAdvice/firstSampleException', 'core.di.factory.example.FirstSampleException'",
+        "'/test/controllerAdvice/secondSampleException', 'core.di.factory.example.SecondSampleException'",
+        "'/test/controllerAdvice/thirdSampleException', 'core.di.factory.example.ThirdSampleException'",
+    })
+    void exceptionHandlers(String requestURI, String targetClassName) throws Exception {
+        Class<?> targetClass = Class.forName(targetClassName);
+        assertExceptionHandler(requestURI, targetClass);
     }
 
-    @Test
-    void multipleExceptionHandlers() throws Exception {
-        String requestURI = "/test/controllerAdvice/secondSampleException";
-        Class<? extends Throwable> targetThrowable = SecondSampleException.class;
 
-        assertExceptionHandler(requestURI, targetThrowable);
-    }
-
-    @Test
-    void multipleExceptionHandlers2() throws Exception {
-        String requestURI = "/test/controllerAdvice/thirdSampleException";
-        Class<? extends Throwable> targetThrowable = ThirdSampleException.class;
-
-        assertExceptionHandler(requestURI, targetThrowable);
-    }
-
-    private void assertExceptionHandler(String requestURI, Class<? extends Throwable> targetThrowable) throws ServletException, IOException {
+    private void assertExceptionHandler(String requestURI, Class<?> targetClass) throws ServletException, IOException {
         request.setRequestURI(requestURI);
         request.setMethod("GET");
         response = new MockHttpServletResponse();
@@ -89,6 +81,6 @@ public class ControllerAdviceTest {
         log.debug("response body : {}", responseBody);
 
         assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
-        assertThat(responseBody).isEqualTo("\"" + targetThrowable.getSimpleName() + "\"");
+        assertThat(responseBody).isEqualTo("\"" + targetClass.getSimpleName() + "\"");
     }
 }
