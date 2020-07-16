39f86fb5c2eed5a54f850a7de4dde18aac7a7f46
ka0oll <jjyoung0216@naver.com>
2020-07-16 21:18:01 +0900

    feat targetProxyFactoryBean구현

--- src/test/java/core/di/beans/factory/support/Echo.java
+++ src/test/java/core/di/beans/factory/support/Echo.java
@@ -5,5 +5,7 @@ public class Echo {
     public String echo(String message){
         return message;
     }
-
+    public String echo2(String message){
+        return message;
+    }
 }
--- src/test/java/core/di/beans/factory/support/ProxyFactoryBeanTest.java
+++ src/test/java/core/di/beans/factory/support/ProxyFactoryBeanTest.java
@@ -6,12 +6,13 @@ import core.aop.Advice;
 import core.aop.Advisor;
 import core.aop.Pointcut;
 import core.di.beans.factory.ProxyFactoryBean;
+import core.di.beans.factory.TargetProxyFactoryBean;
 import org.junit.jupiter.api.Test;
 
 class ProxyFactoryBeanTest {
 
     @Test
-    void uppperProxyFactoryBean() throws Exception {
+    void upperProxyFactoryBean() throws Exception {
         Pointcut pointcut = (method, targetClass, args) -> method.getName().equals("echo");
         Advice advice = invocation -> invocation.proceed().toString().toUpperCase();
 
@@ -21,6 +22,20 @@ class ProxyFactoryBeanTest {
         Echo echo = proxyFactoryBean.getObject();
 
         assertThat(echo.echo("hi")).isEqualTo("HI");
+        assertThat(echo.echo2("hi")).isEqualTo("hi");
     }
 
+    @Test
+    void upperTargetProxyFactoryBean() throws Exception {
+        Pointcut pointcut = (method, targetClass, args) -> method.getName().equals("echo");
+        Advice advice = invocation -> invocation.proceed().toString().toUpperCase();
+
+        Advisor uppper = new Advisor(pointcut, advice);
+        TargetProxyFactoryBean<Echo> proxyFactoryBean = new TargetProxyFactoryBean(new Echo(), uppper);
+
+        Echo echo = proxyFactoryBean.getObject();
+
+        assertThat(echo.echo("hi")).isEqualTo("HI");
+        assertThat(echo.echo2("hi")).isEqualTo("hi");
+    }
 }
