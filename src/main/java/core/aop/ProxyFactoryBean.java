4ea2a438f23d68e22c773bd7bf5290df508b3d9d
ka0oll <jjyoung0216@naver.com>
2020-07-17 23:42:44 +0900

    feat proxybeanfactory multi advisor 적용

---
+++ src/main/java/core/aop/ProxyFactoryBean.java
@@ -0,0 +1,24 @@
+package core.aop;
+
+import core.di.beans.factory.FactoryBean;
+import java.util.List;
+import net.sf.cglib.proxy.Enhancer;
+
+public class ProxyFactoryBean<T> implements FactoryBean<T> {
+
+    private T target;
+    private List<Advisor> advisors;
+
+    public ProxyFactoryBean(T target, List<Advisor> advisors) {
+        this.target = target;
+        this.advisors = advisors;
+    }
+
+    @Override
+    public T getObject() throws Exception {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(this.target.getClass());
+        enhancer.setCallback(new ProxyMethodInterceptor(this.target, this.advisors));
+        return (T) enhancer.create();
+    }
+}
--- src/test/java/core/di/beans/factory/support/ProxyFactoryBeanTest.java
+++ src/test/java/core/di/beans/factory/support/ProxyFactoryBeanTest.java
@@ -5,37 +5,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import core.aop.Advice;
 import core.aop.Advisor;
 import core.aop.Pointcut;
-import core.di.beans.factory.ProxyFactoryBean;
-import core.di.beans.factory.TargetProxyFactoryBean;
+import core.aop.ProxyFactoryBean;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class ProxyFactoryBeanTest {
 
     @Test
-    void upperProxyFactoryBean() throws Exception {
-        Pointcut pointcut = (method, targetClass, args) -> method.getName().equals("echo");
-        Advice advice = invocation -> invocation.proceed().toString().toUpperCase();
-
-        Advisor uppper = new Advisor(pointcut, advice);
-        ProxyFactoryBean<Echo> proxyFactoryBean = new ProxyFactoryBean(Echo.class, new Object[]{}, uppper);
-
-        Echo echo = proxyFactoryBean.getObject();
-
-        assertThat(echo.echo("hi")).isEqualTo("HI");
-        assertThat(echo.echo2("hi")).isEqualTo("hi");
-    }
-
-    @Test
     void upperTargetProxyFactoryBean() throws Exception {
         Pointcut pointcut = (method, targetClass, args) -> method.getName().equals("echo");
-        Advice advice = invocation -> invocation.proceed().toString().toUpperCase();
+        Advice add1 = invocation -> invocation.proceed().toString()+"1";
+        Advice add2 = invocation -> invocation.proceed().toString().toUpperCase()+"2";
 
-        Advisor uppper = new Advisor(pointcut, advice);
-        TargetProxyFactoryBean<Echo> proxyFactoryBean = new TargetProxyFactoryBean(new Echo(), uppper);
+
+        ProxyFactoryBean<Echo> proxyFactoryBean = new ProxyFactoryBean(new Echo(), Arrays.asList(new Advisor(pointcut, add1), new Advisor(pointcut, add2)));
 
         Echo echo = proxyFactoryBean.getObject();
 
-        assertThat(echo.echo("hi")).isEqualTo("HI");
+        assertThat(echo.echo("hi")).isEqualTo("hi21");
         assertThat(echo.echo2("hi")).isEqualTo("hi");
     }
 }
