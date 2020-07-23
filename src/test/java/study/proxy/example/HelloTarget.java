e9ec9c40bee7c30c1c4818aa1f4e163769776040
kjm <jm.kang@Qualson.com>
2020-07-23 18:29:30 +0900

    pingpong 함수 추가

--- src/test/java/study/proxy/example/HelloTarget.java
+++ src/test/java/study/proxy/example/HelloTarget.java
@@ -16,4 +16,9 @@ public class HelloTarget implements Hello {
     public String sayThankYou(String name) {
         return "Thank you " + name;
     }
+
+    @Override
+    public String pingpong(String name) {
+        return name;
+    }
 }
