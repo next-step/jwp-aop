8235b66fd0668885b230e8bdc0dcbe9082a03880
kjm <wjdanr89@gmail.com>
2020-07-23 19:13:13 +0900

    class 매핑 변경

--- src/test/java/study/proxy/example/cglib/MethodCallLogInterceptor.java
+++ src/test/java/study/proxy/example/cglib/MethodCallLogInterceptor.java
@@ -12,7 +12,7 @@ public class MethodCallLogInterceptor implements MethodInterceptor {
         Object returnValue = proxy.invokeSuper(obj, args);
 
         TextUpperCaseMethodMatcher matcher = new TextUpperCaseMethodMatcher("say", "talk");
-        if (matcher.matches(method, obj.getClass(), args)) {
+        if (matcher.matches(method, method.getDeclaringClass(), args)) {
             returnValue = ((String) returnValue).toUpperCase();
         }
 
