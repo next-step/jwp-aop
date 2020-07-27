26debaa1ea73fe9c3afca3643de060d486447473
wbluke <pwb0127@gmail.com>
2020-07-28 00:46:07 +0900

    refactor: ControllerAdviceExceptionMapping 세부 메소드 분리

--- src/main/java/core/mvc/ControllerAdviceExceptionMapping.java
+++ src/main/java/core/mvc/ControllerAdviceExceptionMapping.java
@@ -32,21 +32,28 @@ public class ControllerAdviceExceptionMapping implements ExceptionMapping {
 
         Set<Class<?>> controllerAdviceClasses = reflections.getTypesAnnotatedWith(CONTROLLER_ADVICE_ANNOTATION, true);
         for (Class<?> controllerAdviceClass : controllerAdviceClasses) {
-            List<Method> exceptionHandlerMethods = Arrays.stream(controllerAdviceClass.getDeclaredMethods())
-                    .filter(method -> method.isAnnotationPresent(EXCEPTION_HANDLER_ANNOTATION))
-                    .collect(Collectors.toList());
-
-            for (Method exceptionHandlerMethod : exceptionHandlerMethods) {
-                Class<? extends Throwable> exceptionClass = exceptionHandlerMethod.getAnnotation(EXCEPTION_HANDLER_ANNOTATION).exception();
-
-                handlers.put(exceptionClass, new DefaultExceptionHandler(exceptionHandlerMethod, argumentMatcher));
-            }
+            registerExceptionHandler(controllerAdviceClass);
         }
     }
 
     @Override
     public ExceptionHandler getHandler(Class<? extends Exception> exceptionClass) {
         return handlers.get(exceptionClass);
+    }
+
+    private void registerExceptionHandler(Class<?> controllerAdviceClass) {
+        List<Method> exceptionHandlerMethods = getExceptionHandlerMethods(controllerAdviceClass);
+        for (Method exceptionHandlerMethod : exceptionHandlerMethods) {
+            Class<? extends Throwable> exceptionClass = exceptionHandlerMethod.getAnnotation(EXCEPTION_HANDLER_ANNOTATION).exception();
+
+            handlers.put(exceptionClass, new DefaultExceptionHandler(exceptionHandlerMethod, argumentMatcher));
+        }
+    }
+
+    private List<Method> getExceptionHandlerMethods(Class<?> controllerAdviceClass) {
+        return Arrays.stream(controllerAdviceClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(EXCEPTION_HANDLER_ANNOTATION))
+                .collect(Collectors.toList());
     }
 
 }
