b19dab10faaf92fe863d56271e37cc1414f6a915
wbluke <pwb0127@gmail.com>
2020-07-27 23:00:57 +0900

    refactor: 리팩토링 시 적용했던 주석 제거

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -8,19 +8,9 @@ import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-//    private static final Map<Method, MethodParameter[]> methodParameterCache = new ConcurrentHashMap<>();
-//    private List<ArgumentResolver> argumentResolvers;
-//    private ParameterNameDiscoverer parameterNameDiscoverer;
     private ArgumentMatcher argumentMatcher;
     private Object target;
     private Method method;
-
-//    public HandlerExecution(ParameterNameDiscoverer parameterNameDiscoverer, List<ArgumentResolver> argumentResolvers, Object target, Method method) {
-//        this.parameterNameDiscoverer = parameterNameDiscoverer;
-//        this.argumentResolvers = argumentResolvers;
-//        this.target = target;
-//        this.method = method;
-//    }
 
     public HandlerExecution(ArgumentMatcher argumentMatcher, Object target, Method method) {
         this.argumentMatcher = argumentMatcher;
@@ -38,35 +28,5 @@ public class HandlerExecution {
 
         return (ModelAndView) method.invoke(target, arguments);
     }
-
-//    private MethodParameter[] getMethodParameters() {
-//        MethodParameter[] methodParameters = methodParameterCache.get(method);
-//        String[] parameterNames = parameterNameDiscoverer.getParameterNames(method);
-//
-//        if (methodParameters == null) {
-//            methodParameters = new MethodParameter[method.getParameterCount()];
-//            Class<?>[] parameterTypes = method.getParameterTypes();
-//            Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-//
-//            for (int i = 0; i < methodParameters.length; i++) {
-//                methodParameters[i] = new MethodParameter(method, parameterTypes[i], parameterAnnotations[i], parameterNames[i]);
-//            }
-//
-//            methodParameterCache.put(method, methodParameters);
-//        }
-//
-//        return methodParameters;
-//    }
-//
-//    private Object getArguments(MethodParameter methodParameter, HttpServletRequest request, HttpServletResponse response) {
-//        for (ArgumentResolver resolver : argumentResolvers) {
-//            if (resolver.supports(methodParameter)) {
-//                return resolver.resolveArgument(methodParameter, request, response);
-//            }
-//        }
-//
-//        throw new IllegalStateException("No suitable resolver for argument: " + methodParameter.getType());
-//    }
-
 
 }
