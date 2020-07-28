d0dfddf95e38fd4a18241b089c709a4558a46106
kjm <wjdanr89@gmail.com>
2020-07-28 14:19:54 +0900

    factory bean 테스트 통과

--- src/test/java/core/di/beans/factory/support/proxy/example/HelloFactoryBean.java
+++ src/test/java/core/di/beans/factory/support/proxy/example/HelloFactoryBean.java
@@ -1,11 +1,17 @@
 package core.di.beans.factory.support.proxy.example;
 
 import core.di.beans.factory.proxy.FactoryBean;
+import net.sf.cglib.proxy.Enhancer;
 import study.proxy.example.Hello;
+import study.proxy.example.HelloTarget;
+import study.proxy.example.cglib.MethodCallLogInterceptor;
 
 public class HelloFactoryBean implements FactoryBean<Hello> {
     @Override
     public Hello getObject() throws Exception {
-        return null;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloTarget.class);
+        enhancer.setCallback(new MethodCallLogInterceptor());
+        return (Hello) enhancer.create();
     }
 }
