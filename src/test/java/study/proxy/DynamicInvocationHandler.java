d4224311be4c0a9fa443eb08efa44be3d51e9339
sjgwon <sjgwon@woowahan.com>
2022-07-19 20:54:03 +0900

    feat: Java Dynamic Proxy 적용

---
+++ src/test/java/study/proxy/DynamicInvocationHandler.java
@@ -0,0 +1,26 @@
+package study.proxy;
+
+import com.google.common.collect.Maps;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+
+public class DynamicInvocationHandler implements InvocationHandler {
+    private final Object target;
+    private final Map<String, Method> methods = Maps.newHashMap();
+
+    public DynamicInvocationHandler(Object target) {
+        this.target = target;
+        Arrays.stream(target.getClass().getDeclaredMethods())
+                .forEach(it -> this.methods.put(it.getName(), it));
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        String result = (String) methods.get(method.getName()).invoke(target, args);
+        return result.toUpperCase();
+    }
+}
