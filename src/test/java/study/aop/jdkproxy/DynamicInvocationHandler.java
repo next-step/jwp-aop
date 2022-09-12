2cd485f2e1e4bb8ef817c597acbf1b4e8bb749e4
Jeongsu Yang <mycat83@gmail.com>
2022-09-12 14:14:31 +0900

    1단계 - JDK Proxy와 CGLib Proxy (#63)
    * feat: Java Dynamic Proxy 적용
    
    * feat: CGLib Proxy 적용
    
    * feat: proxy 조건 추가
    
    - pingPong 메서드 추가
    - say로 시작하는 메서드만 대문자로 변환

---
+++ src/test/java/study/aop/jdkproxy/DynamicInvocationHandler.java
@@ -0,0 +1,30 @@
+package study.aop.jdkproxy;
+
+import study.aop.SayMethodMatcher;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamicInvocationHandler implements InvocationHandler {
+	private final Object target;
+	private final Map<String, Method> methods = new HashMap<>();
+
+	public DynamicInvocationHandler(Object target) {
+		this.target = target;
+		Arrays.stream(target.getClass().getDeclaredMethods())
+			.forEach(method -> this.methods.put(method.getName(), method));
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		System.out.printf("invoke method name: %s, args: %s%n", method.getName(), Arrays.toString(args));
+		final Object result = methods.get(method.getName()).invoke(target, args);
+		if (new SayMethodMatcher().matches(method, target.getClass(), args)) {
+			return result.toString().toUpperCase();
+		}
+		return result;
+	}
+}
