e8241f64087c8b91f0065cbbd817037181a8649b
Jeongsu Yang <mycat83@gmail.com>
2022-09-12 23:13:49 +0900

    feat: SayMethodMatcher 상속

--- src/test/java/study/aop/cglibproxy/CustomMethodInterceptor.java
+++ src/test/java/study/aop/cglibproxy/CustomMethodInterceptor.java
@@ -7,12 +7,12 @@ import study.aop.SayMethodMatcher;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
-public class CustomMethodInterceptor implements MethodInterceptor {
+public class CustomMethodInterceptor extends SayMethodMatcher implements MethodInterceptor {
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
         System.out.printf("invoke method name: %s, args: %s%n", method.getName(), Arrays.toString(args));
         final Object result = proxy.invokeSuper(obj, args);
-        if (new SayMethodMatcher().matches(method, obj.getClass(), args)) {
+        if (matches(method, obj.getClass(), args)) {
             return result.toString().toUpperCase();
         }
         return result;
