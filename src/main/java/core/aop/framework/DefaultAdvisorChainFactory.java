152bca6e0e327d96f86973892bf7c7a74c9bf997
seondongpyo <seondongpyo@gmail.com>
2022-09-16 21:06:17 +0900

    refactor: 사용하지 않는 코드 주석 삭제

--- src/main/java/core/aop/framework/DefaultAdvisorChainFactory.java
+++ src/main/java/core/aop/framework/DefaultAdvisorChainFactory.java
@@ -1,7 +1,6 @@
 package core.aop.framework;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +14,6 @@ public class DefaultAdvisorChainFactory implements AdvisorChainFactory {
     @Override
     public List<Advice> getAdvices(Advised config, Method method, Class<?> targetClass) {
         Class<?> actualClass = (targetClass != null) ? targetClass : method.getDeclaringClass();
-
         return Arrays.stream(config.getAdvisors())
             .filter(PointcutAdvisor.class::isInstance)
             .map(PointcutAdvisor.class::cast)
@@ -23,20 +21,5 @@ public class DefaultAdvisorChainFactory implements AdvisorChainFactory {
             .filter(advisor -> advisor.getPointcut().getMethodMatcher().matches(method, actualClass))
             .map(Advisor::getAdvice)
             .collect(Collectors.toList());
-
-        // List<Advice> advices = new ArrayList<>();
-        // Advisor[] advisors = config.getAdvisors();
-        // for (Advisor advisor : advisors) {
-        //     if (advisor instanceof PointcutAdvisor) {
-        //         PointcutAdvisor pointcutAdvisor = (PointcutAdvisor) advisor;
-        //         if (pointcutAdvisor.getPointcut().getClassFilter().matches(actualClass)) {
-        //             MethodMatcher matcher = pointcutAdvisor.getPointcut().getMethodMatcher();
-        //             if (matcher.matches(method, actualClass)) {
-        //                 advices.add(advisor.getAdvice());
-        //             }
-        //         }
-        //     }
-        // }
-        // return advices;
     }
 }
