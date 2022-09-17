a09a60034a6576408b98c54c62cfa440810a3957
wu2ee <gulb1602@gmail.com>
2022-09-17 18:51:08 +0900

    feat : CGLIB Proxy 적용

---
+++ src/test/java/study/cglib/HelloCglibProxyTest.java
@@ -0,0 +1,25 @@
+package study.cglib;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class HelloCglibProxyTest {
+    @Test
+    @DisplayName("Cglib Proxy 학습 테스트")
+    void cglibProxyTest() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloTarget.class);
+        enhancer.setCallback(new HelloUpperCaseMethodInterceptor(new HelloTarget()));
+        HelloTarget helloProxy = (HelloTarget) enhancer.create();
+
+        assertAll(
+                () -> assertThat(helloProxy.sayHello("wu2ee")).isEqualTo("HELLO WU2EE"),
+                () -> assertThat(helloProxy.sayHi("wu2ee")).isEqualTo("HI WU2EE"),
+                () -> assertThat(helloProxy.sayThankYou("wu2ee")).isEqualTo("THANK YOU WU2EE")
+        );
+    }
+}
---
+++ src/test/java/study/cglib/HelloUpperCaseMethodInterceptor.java
@@ -0,0 +1,25 @@
+package study.cglib;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class HelloUpperCaseMethodInterceptor implements MethodInterceptor {
+    private final Object target;
+
+    public HelloUpperCaseMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        Object invokeResult = methodProxy.invoke(target, objects);
+
+        if (invokeResult instanceof String) {
+            return ((String) invokeResult).toUpperCase();
+        }
+
+        return invokeResult;
+    }
+}
