736d171328d0d24147a6ee9b24f512281e5beb98
itdar <thesockerr@gmail.com>
2022-09-18 21:52:06 +0900

    feat: 빈컨테이너의 빈과 프록시 연결 구현
    - DefaultBeanFactory 에 프록시 기능 추가한 ProxyBeanFactory 임시 추가

---
+++ src/main/java/core/di/beans/factory/FactoryBean.java
@@ -0,0 +1,11 @@
+package core.di.beans.factory;
+
+public interface FactoryBean<T> {
+
+    T getObject() throws Exception;
+
+    Class<? extends T> getObjectType();
+
+    boolean isSingleton();
+
+}
---
+++ src/test/java/study/aop/HelloFactoryBean.java
@@ -0,0 +1,32 @@
+package study.aop;
+
+import core.annotation.Component;
+import core.di.beans.factory.FactoryBean;
+import net.sf.cglib.proxy.Enhancer;
+
+@Component
+public class HelloFactoryBean implements FactoryBean<Hello> {
+
+    public HelloFactoryBean() {
+    }
+
+    @Override
+    public Hello getObject() throws Exception {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloFactoryBeanTarget.class);
+        enhancer.setCallback(new HelloTargetCglibProxyTest(new MethodMatcherUpperCaseStartsWith()));
+
+        return (HelloFactoryBeanTarget) enhancer.create();
+    }
+
+    @Override
+    public Class<? extends Hello> getObjectType() {
+        return Hello.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+
+}
