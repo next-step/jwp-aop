6358e44b979a44c3a85e508c87eb78c6be09ba11
seondongpyo <seondongpyo@gmail.com>
2022-09-20 20:24:00 +0900

    refactor: 생성자를 갖는 빈의 프록시 생성 로직 리팩토링

--- src/main/java/core/aop/framework/CglibAopProxy.java
+++ src/main/java/core/aop/framework/CglibAopProxy.java
@@ -32,22 +32,27 @@ public class CglibAopProxy implements AopProxy {
 
         Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(targetClass);
         if (constructor != null) {
-            BeanFactory beanFactory = this.advised.getBeanFactory();
             Class<?>[] parameterTypes = constructor.getParameterTypes();
-
-            List<Object> arguments = new ArrayList<>();
-            for (Class<?> parameterType : parameterTypes) {
-                Object argument = beanFactory.getBean(parameterType);
-                if (argument == null) {
-                    throw new IllegalArgumentException();
-                }
-                arguments.add(argument);
-            }
-
-            return enhancer.create(parameterTypes, arguments.toArray());
+            Object[] arguments = getArguments(parameterTypes);
+            return enhancer.create(parameterTypes, arguments);
         }
 
         return enhancer.create();
+    }
+
+    private Object[] getArguments(Class<?>[] parameterTypes) {
+        BeanFactory beanFactory = this.advised.getBeanFactory();
+
+        List<Object> arguments = new ArrayList<>();
+        for (Class<?> parameterType : parameterTypes) {
+            Object argument = beanFactory.getBean(parameterType);
+            if (argument == null) {
+                throw new IllegalStateException("빈이 존재하지 않습니다: " + parameterType);
+            }
+            arguments.add(argument);
+        }
+
+        return arguments.toArray();
     }
 
     private static class DynamicAdvisedInterceptor implements MethodInterceptor {
