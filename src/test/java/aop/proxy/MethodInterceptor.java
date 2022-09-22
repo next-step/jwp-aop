4b531031396571785468328c4cdc4d1c670758f5
hwangjeonghyeon1 <deert202@naver.com>
2022-09-22 11:49:01 +0900

    feat CGLib Proxy 적용

--- src/test/java/aop/proxy/MethodInterceptor.java
+++ src/test/java/aop/proxy/MethodInterceptor.java
@@ -1,2 +1,28 @@
-package aop.proxy;public class MethodInterceptor {
+package aop.proxy;
+
+import aop.methodMatcher.MethodMatcher;
+import net.sf.cglib.proxy.MethodProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+
+public class MethodInterceptor implements net.sf.cglib.proxy.MethodInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodInterceptor.class);
+    private final MethodMatcher methodMatcher;
+
+    public MethodInterceptor(MethodMatcher methodMatcher) {
+        this.methodMatcher = methodMatcher;
+    }
+
+    @Override
+    public Object intercept(Object target, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        log.debug("method name = {}, param = {}", method.getName(), args[0]);
+        if (methodMatcher.matches(method, target.getClass(), args)) {
+            return proxy.invokeSuper(target, args).toString().toUpperCase();
+        }
+
+        return proxy.invokeSuper(target, args);
+    }
 }
