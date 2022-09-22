edc9761c807bbbff4f9341ccb98eba4cb34d6397
hwangjeonghyeon1 <deert202@naver.com>
2022-09-22 11:48:47 +0900

    feat Java Dynamic Proxy 적용

--- src/test/java/aop/proxy/DynamicInvocationHandler.java
+++ src/test/java/aop/proxy/DynamicInvocationHandler.java
@@ -1,5 +1,6 @@
-package aop;
+package aop.proxy;
 
+import aop.methodMatcher.MethodMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +12,13 @@ import java.util.Map;
 public class DynamicInvocationHandler implements InvocationHandler {
 
     private static final Logger log = LoggerFactory.getLogger(DynamicInvocationHandler.class);
+    private final MethodMatcher methodMatcher;
     private Object target;
     private final Map<String, Method> methods = new HashMap<>();
 
-    public DynamicInvocationHandler(Object target) {
+    public DynamicInvocationHandler(Object target, MethodMatcher methodMatcher) {
         this.target = target;
-
+        this.methodMatcher = methodMatcher;
         Method[] declaredMethods = target.getClass().getDeclaredMethods();
         for (Method declaredMethod : declaredMethods) {
             methods.put(declaredMethod.getName(), declaredMethod);
@@ -25,9 +27,12 @@ public class DynamicInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        log.debug("method name : {} arg {}", args[0]);
+        log.debug("Invoke method name : {}, args : {}", method.getName(), args[0]);
         Object invoke = methods.get(method.getName()).invoke(target, args);
 
-        return invoke.toString().toUpperCase();
+        if (methodMatcher.matches(method, proxy.getClass(), args)) {
+            return invoke.toString().toUpperCase();
+        }
+        return invoke;
     }
 }
