7a2d14c8b7dc67635abc96beb065df5b42daa81e
seondongpyo <seondongpyo@gmail.com>
2022-09-24 16:51:28 +0900

    feat: 예외 처리 로직 추가

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
+import core.mvc.tobe.ExceptionHandlerMapping;
+import core.mvc.tobe.HandlerExecution;
+
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
@@ -21,12 +24,18 @@ public class DispatcherServlet extends HttpServlet {
 
     private HandlerExecutor handlerExecutor = new HandlerExecutor(handlerAdapterRegistry);
 
+    private ExceptionHandlerMapping exceptionHandlerMapping;
+
     public void addHandlerMapping(HandlerMapping handlerMapping) {
         handlerMappingRegistry.addHandlerMpping(handlerMapping);
     }
 
     public void addHandlerAdapter(HandlerAdapter handlerAdapter) {
         handlerAdapterRegistry.addHandlerAdapter(handlerAdapter);
+    }
+
+    public void setExceptionHandlerMapping(ExceptionHandlerMapping exceptionHandlerMapping) {
+        this.exceptionHandlerMapping = exceptionHandlerMapping;
     }
 
     @Override
@@ -45,13 +54,23 @@ public class DispatcherServlet extends HttpServlet {
             ModelAndView mav = handlerExecutor.handle(req, resp, maybeHandler.get());
             render(mav, req, resp);
         } catch (Throwable e) {
-            logger.error("Exception : {}", e);
-            throw new ServletException(e.getMessage());
+            processHandlerException(req, resp, e);
         }
     }
 
     private void render(ModelAndView mav, HttpServletRequest req, HttpServletResponse resp) throws Exception {
         View view = mav.getView();
         view.render(mav.getModel(), req, resp);
+    }
+
+    private void processHandlerException(HttpServletRequest request, HttpServletResponse response, Throwable throwable) throws ServletException {
+        try {
+            HandlerExecution exceptionHandler = exceptionHandlerMapping.getHandler(throwable);
+            ModelAndView mav = exceptionHandler.handle(request, response);
+            render(mav, request, response);
+        } catch (Exception e) {
+            logger.error("Exception : {}", e.getMessage(), e);
+            throw new ServletException(e);
+        }
     }
 }
