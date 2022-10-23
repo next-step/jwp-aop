40d5dbefaf876fbabd3dd907dcedcedcf07f1e76
wu2ee <gulb1602@gmail.com>
2022-10-24 02:57:01 +0900

    refactor : (트랜잭션 테스트) given 작업에 대한 초기 셋팅을 주석을 통해 가독성 향상

--- src/test/java/core/transaction/TransactionalTest.java
+++ src/test/java/core/transaction/TransactionalTest.java
@@ -18,6 +18,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class TransactionalTest {
     private QnaService qnaService;
 
+    /**
+     * DBInitializer -> jwp.sql 초기 데이터 셋팅
+     * Question : id -> 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L (총 8개 데이터 존재)
+     */
     @BeforeEach
     void setUp() {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfiguration.class);
@@ -27,11 +31,14 @@ class TransactionalTest {
     @DisplayName("QnaService 의 addAnswer 메서드가 정상적으로 실행되면 트랜잭션이 commit 된다.")
     @Test
     void commit() throws Exception {
-        long questionId = 1L;
+        // given
+        long questionId = 1L; // 존재하는 질문 id
         Answer answer = new Answer("wu2ee", "만들면서 배우는 Spring", questionId);
 
+        // when
         qnaService.addAnswer(questionId, answer);
 
+        // then
         List<Answer> answers = qnaService.findAllByQuestionId(questionId);
         assertThat(answers).hasSize(1);
     }
@@ -39,8 +46,11 @@ class TransactionalTest {
     @DisplayName("QnaService 의 addAnswer 메서드에서 예외가 발생하면 트랜잭션이 rollback 된다.")
     @Test
     void rollback() {
-        long questionId = 9L;
+        // given
+        long questionId = 9L; // 존재하지 않는 질문 id
         Answer answer = new Answer("wu2ee", "만들면서 배우는 Spring", questionId);
+
+        // when & then
         assertThatThrownBy(() -> qnaService.addAnswer(questionId, answer)).isInstanceOf(DataAccessException.class);
 
         List<Answer> answers = qnaService.findAllByQuestionId(questionId);
@@ -50,8 +60,11 @@ class TransactionalTest {
     @DisplayName("@Transactional 애노테이션을 사용하지 않은 addAnswerNoTransactional 메서드에서 예외가 발생하면 트랜잭션이 rollback 되지 않는다.")
     @Test
     void noRollback() {
-        long questionId = 9L;
+        // given
+        long questionId = 9L; // 존재하지 않는 질문 id
         Answer answer = new Answer("wu2ee", "만들면서 배우는 Spring", questionId);
+
+        // when & then
         assertThatThrownBy(() -> qnaService.addAnswerNoTransactional(questionId, answer)).isInstanceOf(DataAccessException.class);
 
         List<Answer> answers = qnaService.findAllByQuestionId(questionId);
