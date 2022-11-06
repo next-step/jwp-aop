2388b902960cf5f3ef4d9b6217615af497cd7843
hongbin.kim <fusis1@naver.com>
2022-11-06 23:53:57 +0900

    feat: Proxy 팩토리 구현

---
+++ src/main/java/core/di/beans/factory/proxy/cglib/CglibProxyFactory.java
@@ -0,0 +1,38 @@
+package core.di.beans.factory.proxy.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import core.di.beans.factory.proxy.Advisor;
+import core.di.beans.factory.proxy.AdvisorMethodInvocation;
+import core.di.beans.factory.proxy.JoinPoint;
+import core.di.beans.factory.proxy.ProxyFactory;
+
+public class CglibProxyFactory implements ProxyFactory {
+
+    private final Enhancer enhancer = new Enhancer();
+
+    public CglibProxyFactory() {
+    }
+
+    @Override
+    public <T> Object createProxy(Class<T> targetClass, T targetObject, Advisor advisor) {
+        enhancer.setSuperclass(targetClass);
+
+        enhancer.setCallback((MethodInterceptor)(obj, method, args, proxy) -> {
+            JoinPoint joinPoint = () -> invokeSuper(targetObject, args, proxy);
+            return new AdvisorMethodInvocation(advisor, targetClass, method, joinPoint).proceed();
+        });
+
+        return enhancer.create();
+    }
+
+    private Object invokeSuper(Object obj, Object[] objects, MethodProxy proxy) {
+        try {
+            return proxy.invoke(obj, objects);
+        } catch (Throwable e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
